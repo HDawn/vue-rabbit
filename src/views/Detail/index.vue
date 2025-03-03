38c17d79d4bc13a43c1722d661717bf2edc0434c
hehehe <1211106453@qq.com>
2025-03-03 22:57:44 +0800

    '商品详情-基础信息渲染'

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,19 +1,41 @@
 <script setup>
+import { getDetailAPI } from '@/apis/detail';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+
+const detail = ref({})
+
+const getDeatil = async (id = route.params.id) => {
+  const res = await getDetailAPI(id);
+  detail.value = res.result
+}
+onMounted(() => getDeatil())
 
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="detail.categories">
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
+          <!--
+            错误原因：goods一开始{}  {}.categories->undefined->undefined[1]
+            解决办法：1.可选链的语法?.
+                     2.v-1f手动控制演染时机保证只有数据存在才演染
+           -->
+          <el-breadcrumb-item :to="{ path: `/category/${detail.categories[1].id}` }">{{ detail.categories[1].name }}
           </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
+          <el-breadcrumb-item :to="{ path: `/categary/sub/${detail.categories[0].id}` }">{{ detail.categories[0].name }}
           </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <!-- <el-breadcrumb-item :to="{ path: `/category/${detail.categories?.[1].id}` }">{{ detail.categories?.[1].name }}
+          </el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: `/categary/sub/${detail.categories?.[0].id}` }">{{
+            detail.categories?.[0].name }}
+          </el-breadcrumb-item> -->
+          <el-breadcrumb-item>{{ detail.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -27,33 +49,33 @@
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p> {{ detail.salesCount }}+ </p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ detail.commentCount }}+</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ detail.collectCount }}+</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{ detail.brand.name }}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name"> {{ detail.name }} </p>
+              <p class="g-desc">{{ detail.desc }} </p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span> {{ detail.price }}</span>
+                <span>{{ detail.oldPrice }}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -93,13 +115,13 @@
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in detail.details.properties" :key="item.value">
+                      <span class="dt">{{ item.name }}</span>
+                      <span class="dd">{{ item.value }}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                  <img v-for="url in detail.details.pictures" :src="url" :key="url" />
                 </div>
               </div>
             </div>
