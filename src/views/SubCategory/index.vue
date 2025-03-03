8bac9669cc708bec3dd0cc09b9fe6f09005f3f65
hehehe <1211106453@qq.com>
2025-03-03 21:37:44 +0800

    '二级分类-实现列表无限加载'

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -27,6 +27,20 @@ const getGoodsList = async () => {
 }
 onMounted(() => getGoodsList())
 
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodsList()
+}
+const load = async () => {
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodsList.value = [...goodsList.value, ...res.result.items]
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+}
+const disabled = ref(false)
+
 </script>
 
 <template>
@@ -41,12 +55,12 @@ onMounted(() => getGoodsList())
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodsList" :key="goods.id" :goods="goods" />
       </div>
