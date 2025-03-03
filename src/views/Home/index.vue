b6972dbe2cd5a928ba7961e00cc1db1d47d667f6
hehehe <1211106453@qq.com>
2025-03-03 10:57:36 +0800

    'home-使用Panel组件实现新鲜好物'

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,7 +2,7 @@
 import HomeBanner from './components/HomeBanner.vue';
 import HomeCategory from './components/HomeCategory.vue';
 import HomeHot from './components/HomeHot.vue';
-import HomePanel from './components/HomePanel.vue';
+import HomeNew from './components/HomeNew.vue';
 import HomeProduct from './components/HomeProduct.vue';
 </script>
 
@@ -14,10 +14,4 @@ import HomeProduct from './components/HomeProduct.vue';
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <HomePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
-    <div>我是新鲜好物的插槽部分</div>
-  </HomePanel>
-  <HomePanel title="人气推荐" subTitle="新鲜好物 好多商品">
-    <div>我是新鲜好物的插槽部分</div>
-  </HomePanel>
 </template>
