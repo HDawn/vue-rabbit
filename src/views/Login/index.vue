ef12eaccb5c3ca89d0986530797cdbc9790916a2
hehehe <1211106453@qq.com>
2025-03-04 16:28:56 +0800

    '登录-表单校验'

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,21 @@
 <script setup>
+import { ref } from 'vue';
 
+//1.表单
+const form = ref({
+  account: '',
+  password: ''
+})
+//2.规则
+const rules = {
+  account: [
+    { required: true, message: '用户名不能为空', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码长度在6-14之间', trigger: 'blur' }
+  ]
+}
 </script>
 
 
@@ -24,16 +40,15 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
-              status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+                <el-checkbox size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
