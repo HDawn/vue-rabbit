241f13af40b0f52546ac2cdbab14f7e9ab86ce01
hehehe <1211106453@qq.com>
2025-03-04 16:43:22 +0800

    '登录-表单校验-自定义校验规则'

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -4,7 +4,8 @@ import { ref } from 'vue';
 //1.表单
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: false
 })
 //2.规则
 const rules = {
@@ -14,6 +15,18 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', trigger: 'blur' },
     { min: 6, max: 14, message: '密码长度在6-14之间', trigger: 'blur' }
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value);
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('未勾选协议'))
+        }
+      }
+    }
   ]
 }
 </script>
@@ -47,8 +60,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
