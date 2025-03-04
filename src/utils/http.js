cce6a052fd846afbff7d377eafe46811b7161f8c
hehehe <1211106453@qq.com>
2025-03-04 22:27:13 +0800

    '登录-401拦截处理'

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,3 +1,4 @@
+import router from '@/router'
 import { useUserStore } from '@/stores/user'
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
@@ -20,10 +21,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  //统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+
+  //401token失效处理
+  if (e.response.status === 401) {
+    const userStore = useUserStore()
+    userStore.clearUserInfo()
+    router.push("/login")
+    ElMessage({ type: "warning", message: "请重新登录" })
+  }
   return Promise.reject(e)
 })
 
