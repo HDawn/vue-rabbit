b238d0334027333b8e936d5c83eb0fe873db5aaa
hehehe <1211106453@qq.com>
2025-03-04 13:27:58 +0800

    '解决样式错乱的问题'

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-import { lazyPlugin } from './directives'
 
+// 引入初始化样式文件
+import '@/styles/common.scss'
+
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
+// 引入全局组件插件
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+// 注册持久化插件
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.mount('#app')
+
+
 
 
