03d214c9cc8ae9c7bc804cd232b13589b200f0e8
hehehe <1211106453@qq.com>
2025-03-05 14:41:32 +0800

    '本地购物车-实现删除购物车中一条商品/computed计算购物车中商品总数和总价'

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 
 export const useCartStore = defineStore('cart', () => {
@@ -15,9 +15,21 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
+  const delCart = (skuId) => {
+    const idx = cartList.value.findIndex((item) => (item.skuId === skuId))
+    cartList.value.splice(idx, 1)
+  }
+
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+
+
   return {
+    allCount,
+    allPrice,
     cartList,
-    addCart
+    addCart,
+    delCart
   }
 
 }, {
