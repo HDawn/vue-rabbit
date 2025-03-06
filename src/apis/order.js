40bef0aedc0487e161f1043913f3b67ed3266e80
hehehe <1211106453@qq.com>
2025-03-06 22:24:34 +0800

    '会员中心-使用tab切换数据状态'

---
+++ src/apis/order.js
@@ -0,0 +1,16 @@
+import request from "@/utils/http"
+
+/*
+params: {
+  orderState:0,
+  page:1,
+  pageSize:2
+}
+*/
+export const getOrderListAPI = (params) => {
+  return request({
+    url: '/member/order',
+    method: 'GET',
+    params
+  })
+}
