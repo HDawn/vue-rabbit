723de541668f9d314d86a73034b70527f95d4e82
hehehe <1211106453@qq.com>
2025-03-06 16:41:41 +0800

    '结算页-倒计时逻辑函数实现'

---
+++ src/composables/useCountDown.js
@@ -0,0 +1,23 @@
+import dayjs from "dayjs"
+import { computed, onUnmounted, ref } from "vue"
+
+export const useCountDown = () => {
+  const time = ref(0)
+  const formatTime = computed(() => dayjs.unix(time.value).format('mm分ss秒'))
+
+  let timer = null;
+  const start = (currentTime) => {
+    time.value = currentTime
+    timer = setInterval(() => {
+      time.value--;
+    }, 1000)
+  }
+
+  onUnmounted(() => {
+    timer && clearInterval(timer)
+  })
+
+  return {
+    formatTime, start
+  }
+}
