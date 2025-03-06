c4011333629ca8e87dca7f2c9135670a5500a1c7
hehehe <1211106453@qq.com>
2025-03-06 17:15:41 +0800

    '会员中心-功能理解以及路由配置'

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from "@/views/CartList/index.vue"
 import CheckOut from "@/views/CheckOut/index.vue"
 import Pay from "@/views/Pay/index.vue"
 import PayBack from "@/views/Pay/PayBack.vue";
+import Member from "@/views/Member/index.vue"
+import UserOrder from "@/views/Member/components/UserOrder.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,6 +48,18 @@ const router = createRouter({
         }, {
           path: "paycallback",
           component: PayBack
+        }, {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            }, {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
         }
       ],
     },
